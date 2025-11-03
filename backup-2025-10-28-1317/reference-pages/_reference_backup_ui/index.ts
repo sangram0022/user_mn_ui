0be6018ec2352cc62feedde8e8435e2d932da423
sangram <sangram0202@gmail.com>
2025-11-03 11:09:56 +0530

    chore: remove 158 outdated documentation files and backup directories
    - Remove 158 redundant markdown files (WEEK_*, PHASE_*, *_COMPLETE.md, etc.)
    - Remove 4 backup directories (backup-2025-10-28-*, css-backup-*)
    - Remove build.log artifact
    - Keep essential docs: README, ARCHITECTURE, API_ENDPOINT_MAPPING, VALIDATION_ARCHITECTURE
    - Cleanup reduces root directory clutter by 95%

--- backup-2025-10-28-1317/reference-pages/_reference_backup_ui/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Reference Pages Index
- * 
- * Centralized export for all reference pages
- */
-
-export { default as UIElementsShowcase } from './UIElementsShowcase';
-export { default as FormPatternsReference } from './FormPatternsReference';
-export { default as ComponentPatternsReference } from './ComponentPatternsReference';
